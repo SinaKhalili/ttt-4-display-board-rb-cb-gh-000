bcc879ee3562dd670f2657c882540b90d0489a94
sina khalili <khalili@sfu.ca>
2017-04-20 08:13:43 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(p)
+  puts(" #{p[0]} | #{p[1]} | #{p[2]} ")
+  puts "-----------"
+  puts(" #{p[3]} | #{p[4]} | #{p[5]} ")
+  puts "-----------"
+  puts(" #{p[6]} | #{p[7]} | #{p[8]} ")
+end 
